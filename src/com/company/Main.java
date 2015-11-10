2109b9cc990fd9ec2ba649a4873673d4b213925d
Estefania Ortiz <eaortiz@stanford.edu>
2015-11-10 12:56:32 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,19 +12,16 @@ public class Main {
 
     }
 
-
-
-
-    private static boolean isEvenWord(String s)
+    private static boolean isEvenWord(String s) //s could be named better
     {
         if (s.length() == 0)
             return true;
 
-        String search = String.valueOf(s.charAt(0));
+        String search = String.valueOf(s.charAt(0)); // search could have a better variable name
         s = s.substring(1);
 
         if(s.contains(search)) {
-            s = s.replace(search, "");
+            s = s.replace(search, ""); //what if there is more than one of the character 'search'?
             return isEvenWord(s);
 
         }
